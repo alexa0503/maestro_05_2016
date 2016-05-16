e05554fcbeed82cb14c53389fb857853d77c2ad1
alexa0503 <lori.w@live.cn>
2016-05-17 00:51:28 +0800

    modified:   app/Http/Controllers/HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -42,26 +42,29 @@ class HomeController extends Controller
             $thumb_img = str_replace(' ', '+', $thumb_img);
             $thumb_data = base64_decode($thumb_img);
             //$file_name = uniqid() . '.png';
-            $thumb_file = public_path('uploads') . '/share/' . $file_name;
-            file_put_contents($thumb_file, $thumb_data);
-            file_put_contents($img_file, $img_data);
+            $thumb_file = public_path('uploads/share') . '/' . $file_name;
 
-            $photo = new \App\Photo();
-            $photo->sid = $request->session()->getId();
-            $photo->image = $file_name;
-            $photo->attitude = $request->input('attitude');
-            $photo->friend_name = $request->input('friend_name');
-            $photo->self_name = $request->input('self_name');
-            //$photo->created_time = date('Y-m-d H:i:s');
-            $photo->created_time = Carbon::now();
-            $photo->created_ip = $request->getClientIp();
-            $photo->save();
-            $result['data'] = array(
-                'title'=>'快来膜拜我的心造型，一起有型造起来。',
-                'desc'=>'音乐节嗨翻天，有型造起来。',
-                'link'=>url('share',array('id'=>$photo->id)),
-                'imgUrl'=>asset('uploads/share/'.$photo->image),
-            );
+            if( !file_put_contents($img_file, $img_data) || !file_put_contents($thumb_file, $thumb_data)){
+                $result = array('ret'=>1002,'msg'=>'写入失败~');
+            }
+            else{
+                $photo = new \App\Photo();
+                $photo->sid = $request->session()->getId();
+                $photo->image = $file_name;
+                $photo->attitude = $request->input('attitude');
+                $photo->friend_name = $request->input('friend_name');
+                $photo->self_name = $request->input('self_name');
+                //$photo->created_time = date('Y-m-d H:i:s');
+                $photo->created_time = Carbon::now();
+                $photo->created_ip = $request->getClientIp();
+                $photo->save();
+                $result['data'] = array(
+                    'title'=>'快来膜拜我的心造型，一起有型造起来。',
+                    'desc'=>'音乐节嗨翻天，有型造起来。',
+                    'link'=>url('share',array('id'=>$photo->id)),
+                    'imgUrl'=>asset('uploads/share/'.$photo->image),
+                );
+            }
         }
         return json_encode($result);
     }
