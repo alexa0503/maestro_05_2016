8a6804058df84b9bc3c7c616a51282ba2a3466bc
wang4869 <wang4869@139.com>
2016-06-06 23:14:46 +0800

    window update

--- public/js/common.js
+++ public/js/common.js
@@ -36,10 +36,14 @@ $(document).ready(function () {
 		$('.page').css('-webkit-transform','scale('+bli+')');
 		$('.page').css('-webkit-transform-origin','50% 0px');
 		}
-		else{
+		else if(wHeight>1039){
 			$('.page').height(wHeight);
-			$('.h1008').css('padding-top',(wHeight-1008)/2+'px');
+			$('.h1008').css('padding-top',wHeight-1008+'px');
 			}
+			else{
+				$('.page').height(wHeight);
+				$('.h1008').css('padding-top',(wHeight-1008)/2+'px');
+				}
 });
 
 window.onresize=winOnResize;
@@ -56,10 +60,14 @@ function winOnResize(){
 		$('.page').css('-webkit-transform','scale('+bli+')');
 		$('.page').css('-webkit-transform-origin','50% 0px');
 		}
-		else{
+		else if(wHeight>1039){
 			$('.page').height(wHeight);
-			$('.h1008').css('padding-top',(wHeight-1008)/2+'px');
+			$('.h1008').css('padding-top',wHeight-1008+'px');
 			}
+			else{
+				$('.page').height(wHeight);
+				$('.h1008').css('padding-top',(wHeight-1008)/2+'px');
+				}
 	}
 
 var phTxt=[{p1:'个性',p2:'独立摄影师',p3:'Bruce'},{p1:'颠覆',p2:'时尚博主',p3:'Carol'},{p1:'阳光',p2:'乐队鼓手',p3:'Charles'},{p1:'颓废',p2:'摇滚乐手',p3:'Gloria'},{p1:'活力',p2:'是平面模特',p3:'Helena'},{p1:'随性',p2:'青年舞蹈家',p3:'Diana'},{p1:'朋克',p2:'男团主唱',p3:'Eric'}];
